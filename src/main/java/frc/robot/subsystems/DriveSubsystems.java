b6c0c9e0178f549409681987e5e088b1d3ef8465
chriswn <128608842+chriswn@users.noreply.github.com>
2025-01-14 10:44:57 -0600

    Update DriveSubsystems.java
    motors and working on speed controller

--- src/main/java/frc/robot/subsystems/DriveSubsystems.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystems.java
@@ -1,1 +1,39 @@
+package frc.main.subsystems 
 
+
+
+
+  public calss DriveSubsystems extends(subsystems){
+    // Declare motor controllers
+  private final PWMSparkMax leftMotor1 = new PWMSparkMax(0);  // PWM port 0 for left motor
+  private final PWMSparkMax leftMotor2 = new PWMSparkMax(1);  // PWM port 1 for left motor
+  private final PWMSparkMax rightMotor1 = new PWMSparkMax(2); // PWM port 2 for right motor
+  private final PWMSparkMax rightMotor2 = new PWMSparkMax(3); // PWM port 3 for right motor
+
+  // SpeedControllerGroups to group left and right motors
+  private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftMotor1, leftMotor2);
+  private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightMotor1, rightMotor2);
+
+     private final DifferentialDrive m_drive = new DifferentialDrive(m_leftLeader::set, m_rightLeader::set);
+
+    // The left-side drive encoder
+  private final Encoder m_leftEncoder =
+      new Encoder(
+          DriveConstants.kLeftEncoderPorts[0],
+          DriveConstants.kLeftEncoderPorts[1],
+          DriveConstants.kLeftEncoderReversed);
+
+  // The right-side drive encoder
+  private final Encoder m_rightEncoder =
+      new Encoder(
+          DriveConstants.kRightEncoderPorts[0],
+          DriveConstants.kRightEncoderPorts[1],
+          DriveConstants.kRightEncoderReversed);
+
+
+    public DriveSubsystems() {
+private final SpeedcontrollerGroup rightspeedcontroller = new SpeedcontrollerGroup(rightMotor1, rigthMotor2);
+private final SpeedcontrollerGroup leftspeedcontroller = new SpeedcontrollerGroup(lefttMotor1, leftMotor2);
+    }
+
+  }
