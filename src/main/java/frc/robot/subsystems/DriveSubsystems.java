5e68426262baa2c70923ccae97c64722ba8d2074
chriswn <128608842+chriswn@users.noreply.github.com>
2025-01-14 11:00:31 -0600

    Update DriveSubsystems.java
    Full working not tested DriveSubsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystems.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystems.java
@@ -1,10 +1,16 @@
-package frc.main.subsystems 
+package frc.main.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
-
-  public calss DriveSubsystems extends(subsystems){
-    // Declare motor controllers
+public class DriveSubsystem extends SubsystemBase {
+  
+  // Declare motor controllers for left and right sides of the drive
   private final PWMSparkMax leftMotor1 = new PWMSparkMax(0);  // PWM port 0 for left motor
   private final PWMSparkMax leftMotor2 = new PWMSparkMax(1);  // PWM port 1 for left motor
   private final PWMSparkMax rightMotor1 = new PWMSparkMax(2); // PWM port 2 for right motor
@@ -14,26 +20,35 @@ package frc.main.subsystems
   private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftMotor1, leftMotor2);
   private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightMotor1, rightMotor2);
 
-     private final DifferentialDrive m_drive = new DifferentialDrive(m_leftLeader::set, m_rightLeader::set);
+  // DifferentialDrive for controlling the robot
+  private final DifferentialDrive drivetrain = new DifferentialDrive(leftMotors, rightMotors);
 
-    // The left-side drive encoder
-  private final Encoder m_leftEncoder =
-      new Encoder(
-          DriveConstants.kLeftEncoderPorts[0],
-          DriveConstants.kLeftEncoderPorts[1],
-          DriveConstants.kLeftEncoderReversed);
+  // The left and right encoders
+  private final Encoder leftEncoder = new Encoder(0, 1);  
+  private final Encoder rightEncoder = new Encoder(2, 3); 
 
-  // The right-side drive encoder
-  private final Encoder m_rightEncoder =
-      new Encoder(
-          DriveConstants.kRightEncoderPorts[0],
-          DriveConstants.kRightEncoderPorts[1],
-          DriveConstants.kRightEncoderReversed);
+  // Odometry to track robot position
+  private final DifferentialDriveOdometry odometry;
 
+  // Joystick for controlling the robot
+  private final Joystick joystick = new Joystick(0);
 
-    public DriveSubsystems() {
-private final SpeedcontrollerGroup rightspeedcontroller = new SpeedcontrollerGroup(rightMotor1, rigthMotor2);
-private final SpeedcontrollerGroup leftspeedcontroller = new SpeedcontrollerGroup(lefttMotor1, leftMotor2);
-    }
-
+  public DriveSubsystem() {
+    // Initialize odometry with gyro and encoder data (assuming you have a gyro)
+    odometry = new DifferentialDriveOdometry(gyro.getRotation2d(), leftEncoder.getDistance(), rightEncoder.getDistance());
   }
+
+  @Override
+  public void periodic() {
+    // Update odometry periodically
+    odometry.update(gyro.getRotation2d(), leftEncoder.getDistance(), rightEncoder.getDistance());
+  }
+
+  // Arcade drive using joystick input
+  public void arcadeDrive() {
+    double forward = joystick.getY(); // Forward movement (joystick Y-axis)
+    double rotation = joystick.getX(); // Rotation (joystick X-axis)
+    drivetrain.arcadeDrive(forward, rotation);
+  }
+
+}
