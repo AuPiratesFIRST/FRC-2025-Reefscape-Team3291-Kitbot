42b3a1454ef0f6764f8b7a8bf66e7d3a3ad62a68
chriswn <128608842+chriswn@users.noreply.github.com>
2025-01-29 13:08:07 -0600

    Update Intakesystem.java

--- src/main/java/frc/robot/subsystems/Intakesystem.java
+++ src/main/java/frc/robot/subsystems/Intakesystem.java
@@ -5,10 +5,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
+
 
 public class Intakesystem extends SubsystemBase {
   /** Creates a new EmptySubsystem. */
   public Intakesystem() {
+    TalonSRX motor1 = new TalonSRX(8);
+    Joystick exampleJoystick = new Joystick(0);
     
   }
 
