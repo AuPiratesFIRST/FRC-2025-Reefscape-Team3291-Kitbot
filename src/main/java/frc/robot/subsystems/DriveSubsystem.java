98d1f061f8f6b9d1c205678f6182b2e65c637fe7
chriswn <128608842+chriswn@users.noreply.github.com>
2025-02-11 14:52:11 -0600

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -9,15 +9,10 @@ import frc.robot.Constants;
 public class DriveSubsystem extends SubsystemBase {
 
   // Declare motor controllers for left and right sides of the drive using CAN
-  private final VictorSPX leftFrontMotor = new VictorSPX(Constants.DriveConstants.kLeftFrontMotorPort); // CAN ID 1 for
-                                                                                                        // left motor
-  private final VictorSPX leftBackMotor = new VictorSPX(Constants.DriveConstants.kLeftBackMotorPort); // CAN ID 2 for
-                                                                                                      // left motor
-  private final VictorSPX rightFrontMotor = new VictorSPX(Constants.DriveConstants.kRightFrontMotorPort); // CAN ID 3
-                                                                                                          // for right
-                                                                                                          // motor
-  private final VictorSPX rightBackMotor = new VictorSPX(Constants.DriveConstants.kRightBackMotorPort); // CAN ID 4 for
-                                                                                                        // right motor
+  private final CANSparkMax leftFrontMotor = new CANSparkMax(Constants.DriveConstants.kLeftFrontMotorPort, MotorType.kBrushless); // CAN ID 1 for left motor
+  private final CANSparkMax leftBackMotor = new CANSparkMax(Constants.DriveConstants.kLeftBackMotorPort, MotorType.kBrushless); // CAN ID 2 for left motor
+  private final CANSparkMax rightFrontMotor = new CANSparkMax(Constants.DriveConstants.kRightFrontMotorPort, MotorType.kBrushless); // CAN ID 3 for right motor
+  private final CANSparkMax rightBackMotor = new CANSparkMax(Constants.DriveConstants.kRightBackMotorPort, MotorType.kBrushless); // CAN ID 4 for right motor
 
   public DriveSubsystem() {
     // Set the motor inversion based on the constants
@@ -29,19 +24,20 @@ public class DriveSubsystem extends SubsystemBase {
     leftBackMotor.follow(leftFrontMotor); // Set left back motor to follow left front motor
     rightBackMotor.follow(rightFrontMotor); // Set right back motor to follow right front motor
 
-    // Set the motor speed to 0 initially
-    leftFrontMotor.set(VictorSPXControlMode.PercentOutput, Constants.DriveConstants.brakeSpeed);
-    rightFrontMotor.set(VictorSPXControlMode.PercentOutput, Constants.DriveConstants.brakeSpeed);
+    // Set the motor speed to 0 initially (this can be considered as brake mode for SparkMax)
+    leftFrontMotor.set(0);
+    rightFrontMotor.set(0);
   }
 
   // Method to drive using tank drive (left and right joystick values)
   public void tankDrive(double leftSpeed, double rightSpeed) {
     // Set the motor speeds based on joystick input
-    leftFrontMotor.set(VictorSPXControlMode.PercentOutput, leftSpeed * Constants.DriveConstants.maxSpeed);
-    rightFrontMotor.set(VictorSPXControlMode.PercentOutput, rightSpeed * Constants.DriveConstants.maxSpeed);
+    leftFrontMotor.set(leftSpeed * Constants.DriveConstants.maxSpeed);
+    rightFrontMotor.set(rightSpeed * Constants.DriveConstants.maxSpeed);
   }
 
   @Override
   public void periodic() {
+    // Optional: Add any periodic code that needs to run
   }
 }
