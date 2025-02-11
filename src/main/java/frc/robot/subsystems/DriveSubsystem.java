b337d6888b1cacf749e14b014658df4a682448f5
kevin <lopkev5546@apps.district279.org>
2025-02-11 16:41:25 -0600

    help

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,11 +1,21 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com. revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
+SparkMax leftLeader;
+SparkMax leftfollower;
+SparkMax rigthLeader;
+SparkMax rigthfollower;
+
+
 
   // Declare motor controllers for left and right sides of the drive using CAN
   private final SparkMax leftFrontMotor = new SparkMax(Constants.DriveConstants.kLeftFrontMotorPort, MotorType.kBrushless); // CAN ID 1 for left motor
@@ -13,20 +23,45 @@ public class DriveSubsystem extends SubsystemBase {
   private final SparkMax rightFrontMotor = new SparkMax(Constants.DriveConstants.kRightFrontMotorPort, MotorType.kBrushless); // CAN ID 3 for right motor
   private final SparkMax rightBackMotor = new SparkMax(Constants.DriveConstants.kRightBackMotorPort, MotorType.kBrushless); // CAN ID 4 for right motor
 
+  SparkMaxConfig globalConfig = new SparkMaxConfig(); 
+  SparkMaxConfig leftBackMotorConfig = new SparkMaxConfig();
+  SparkMaxConfig rightFrontMotorConfig = new SparkMaxConfig();
+  SparkMaxConfig rightBackMotorConfig = new SparkMaxConfig();
+
+  public SparkMaxConfig getGlobalConfig() {
+      return globalConfig;
+  }
+
+  globalConfig  
+  .setSmartCurrentLimit(50)
+  .idleMode(IdleMode.kBrake);
+
+  leftBackMotorConfig
+  .apply(globalConfig)
+  .inverterted(true);
+
+  rigthLeaderConfig
+  .apply(globalConfig)
+  .inverterted(true);
+
+  rigthfollowerConfig
+  .apply(globalConfig)
+  inverterted(true);
+
+
+
+  @SuppressWarnings("deprecation")
   public DriveSubsystem() {
     // Set the motor inversion based on the constants
-    leftFrontMotor.setInverted(Constants.DriveConstants.leftInverted);
-    leftBackMotor.setInverted(Constants.DriveConstants.leftInverted);
-    rightFrontMotor.setInverted(Constants.DriveConstants.rightInverted);
-    rightBackMotor.setInverted(Constants.DriveConstants.rightInverted);
-
-  // leftBackMotor. addFollower(leftFrontMotor);  
-  leftBackMotor.isFollower();
-  rightBackMotor.isFollower(); 
+    leftFrontMotor.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode. kPersistParameters);
+    leftBackMotor.configure(leftBackMotorConfig, ResetMode.kResetSafeParameters, PersistMode. kPersistParameters);
+    rightFrontMotor.configure(rightFrontMotorConfig, ResetMode.kResetSafeParameters, PersistMode. kPersistParameters);
+    rightBackMotor.configure(rightBackMotorConfig, ResetMode.kResetSafeParameters, PersistMode. kPersistParameters);
 
     // Set the motor speed to 0 initially (this can be considered as brake mode for SparkMax)
     leftFrontMotor.set(0);
     rightFrontMotor.set(0);
+    
   }
 
   // Method to drive using tank drive (left and right joystick values)
