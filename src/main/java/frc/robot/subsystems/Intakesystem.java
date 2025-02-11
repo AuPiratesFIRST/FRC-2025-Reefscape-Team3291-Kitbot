1a19750feaaabfed2ee17d318244125391969d2b
chriswn <128608842+chriswn@users.noreply.github.com>
2025-02-11 14:54:02 -0600

    Update Intakesystem.java

--- src/main/java/frc/robot/subsystems/Intakesystem.java
+++ src/main/java/frc/robot/subsystems/Intakesystem.java
@@ -4,21 +4,22 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intakesystem extends SubsystemBase {
   /** Creates a new Intakesystem. */
 
-  private Talon intakeMotor;
+  private CANSparkMax intakeMotor;
 
   public Intakesystem() {
     // Initialize the motor with the CAN ID (replace 8 with the actual CAN ID of
     // your motor)
-    intakeMotor = new Talon(Constants.IntakeConstants.kIntakeMotorPort); // Replace with the actual CAN ID of your motor
+    intakeMotor = new CANSparkMax(Constants.IntakeConstants.kIntakeMotorPort, MotorType.kBrushless); // Replace with the actual CAN ID of your motor
     intakeMotor.setInverted(Constants.IntakeConstants.kIntakeInverted); // Set motor inversion
-    intakeMotor.set(Constants.IntakeConstants.kIntakeStopSpeed); // Set initial speed to 0
+    intakeMotor.set(0); // Set initial speed to 0
   }
 
   @Override
